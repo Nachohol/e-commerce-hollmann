1b95f9c0dc03ce19cc245617067c435118cb8f64
Ignacio Hollmann <hollmann.ignacio@gmail.com>
2022-01-26 23:52:59 -0300

    clase 02

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import CartWidget from "./icon/CartWidget";
+
+const NavBar = () => {
+    return (
+        <>
+            <Navbar bg="light"  expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">Fast Food E-Commerce</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Inicio</Nav.Link>
+                            <Nav.Link href="#link">Contacto</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <CartWidget href="#home" />
+                </Container>  
+            </Navbar>
+        </>
+    )
+}
+
+export default NavBar
